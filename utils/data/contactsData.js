ffc33225cf147a7fb0fb0a10075e7eb9cc0699cd
DARSHAN A M <amdarshan557@gmail.com>
2025-09-06 11:25:50 +0530

    Update contactsData.js
    contact details has been updated

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -6,6 +6,5 @@ export const contactsData = {
     facebook: 'https://www.facebook.com/lovelydarshan.darshu.96/',
     linkedIn: 'https://www.linkedin.com/in/darshan-a-m/',
     twitter: 'https://x.com/Darshan64273074/',
-    //stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    //devUsername: "said7388"
+
 }
